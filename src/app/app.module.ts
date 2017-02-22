8b5d5abceb174b19559cc7a73a897be58b09a2bb
ugurozturk <tarantulaugur@hotmail.com>
2017-02-22 19:31:23 +0300

    ilk Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/homepage/homepage';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+
+@NgModule({
+  declarations: [
+    MyApp,
+	HomePage,
+    Page1,
+    Page2
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+	HomePage,
+    Page1,
+    Page2
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/homepage/homepage.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-homepage',
+  templateUrl: 'homepage.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
