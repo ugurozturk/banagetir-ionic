8e19b603b94614cf9ee406f4701d033f8553aae6
ugurozturk <tarantulaugur@hotmail.com>
2017-03-04 19:32:45 +0300

    Map hatası düzeltildi

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, Platform, ModalController } from 'ionic-angular';
-import { Geolocation, GoogleMap, GoogleMapsEvent, GoogleMapsLatLng, CameraPosition, GoogleMapsMarkerOptions } from 'ionic-native';
-
 import { AdresEkle } from '../adresekle/adresekle';
 
 @Component({
@@ -9,69 +7,15 @@ import { AdresEkle } from '../adresekle/adresekle';
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
-mapiYukle: boolean;
-adreslerim: string = "2";
+  mapiYukle: boolean;
+  adreslerim: string = "2";
   constructor(public navCtrl: NavController, public plt: Platform, public modalCtrl: ModalController) {
     if (plt.is('android')) {
       this.mapiYukle = true;
     }
-  }
-/*
-  ngAfterViewInit() {
-    if (this.mapiYukle) {
-      this.loadMap();
-    }
- }
-*/
-/*
-loadMap() {
- // make sure to create following structure in your view.html file
- // and add a height (for example 100%) to it, else the map won't be visible
- // <ion-content>
- //  <div #map id="map" style="height:100%;"></div>
- // </ion-content>
+  }//constructor End
 
- // create a new map by passing HTMLElement
- let element: HTMLElement = document.getElementById('map');
-
- let map = new GoogleMap(element);
-
- // listen to MAP_READY event
- map.on(GoogleMapsEvent.MAP_READY);//.then(() => console.log('Map is ready!'));
-
- let ionic: GoogleMapsLatLng;
- //User Location
- var options = {enableHighAccuracy: true};
- Geolocation.getCurrentPosition(options).then((resp) => {
-   console.log("User Location : ");
-  ionic = new GoogleMapsLatLng(resp.coords.latitude, resp.coords.longitude);
-
-   // create CameraPosition
- let position: CameraPosition = {
-   target: ionic,
-   zoom: 18,
-   tilt: 30
- };
-
- // move the map's camera to position
- map.moveCamera(position);
-
-  // create new marker
- let markerOptions: GoogleMapsMarkerOptions = {
-   position: ionic,
-   draggable: true,
-   icon: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QIXES46di1HGAAAAS1JREFUOMul1DFuFEEQheGvGwLEnIGoiZxYgIWQ+hAb7UFIiQmJ8AGIWZP7BJNZ4MAyyNJkPoIbr1aIdVIjBoPQsvNHrVbpdVX1q0omdP2g1aLrh0d4ied4goRrfMZZq+VmjB1Jo8CEBY6xxtbvJDzAG3ycCuXxEJcrvMPtX0TE3Q+8xek0iTTJZoUXEbgLGeetlmXXD3JkssCz/xCBnzjq+uF1q0WKxl5FOftwi1c5fmdtfx7jMMcXb81jmcMnc9jiII9emknK4dhZIviaw/ZpptAq4yxsvy/fcZ5bLTcxO3kPkYc4abWspyNyiqfh2F351mpZuN+brh9OcITNDplcYIlNq+VXObFflngftk/3HkpR/hofIpPNuErSPxbbYbx4EHGX+IQvrZY/RuoOtQ1ieqVTJ4EAAAAASUVORK5CYII=",
-   title: 'Konumum'
- };
-
- map.addMarker(markerOptions);
-
-}).catch((error) => {
-  console.log('Error getting location', error);
-});
-}*/
-
-  adresChanged(event:any){
+  adresChanged(event: any) {
     if (event == "0") {
       this.presentModal();
       console.log("Yeni Adres");
@@ -85,8 +29,6 @@ loadMap() {
     let modal = this.modalCtrl.create(AdresEkle);
     modal.present();
   }
-
-
-}
+}//Class ProfilePage End
 
 
